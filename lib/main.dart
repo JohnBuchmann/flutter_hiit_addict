89562fcb8a33cb239c6815936b3dcccd29f05b13
John Buchmann <john@integro212.com>
2024-11-21 22:23:22 -0500

    fix compile errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hiit_addict/firebase_options.dart';
 import 'screens/home_screen.dart';
 import 'app_theme.dart';
 import 'providers/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
